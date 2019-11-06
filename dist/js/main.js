4bee0dc9170120740045ef90166582cab968c6fc
Jason Diep <jason_diep@hotmail.co.uk>
2019-11-06 00:37:08 +0000

    add page transition and remove menu portrait

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,18 @@
-// Select DOM Items
+const swup = new Swup();
 
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+// Menu Bar
 
-// Set Initial State of Menu
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
 
-let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+navItems.forEach(item => item.addEventListener("click", toggleMenu));
 
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu(){
-    menuBtn.classList.toggle('close');
-    menu.classList.toggle('show');
-    menuNav.classList.toggle('show');
-    menuBranding.classList.toggle('show');
-    navItems.forEach(item=>item.classList.toggle('show'));
-    showMenu = !showMenu;
+function toggleMenu() {
+    menuBtn.classList.toggle("close");
+    menu.classList.toggle("show");
+    menuNav.classList.toggle("show");
+    navItems.forEach(item => item.classList.toggle("show"));
 }
